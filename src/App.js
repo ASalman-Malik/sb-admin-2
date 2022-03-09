c2c106964910e432fbcb0a7d415d0e4255cf8030
Ahmad Salman Malik <malik.salman96@gmail.com>
2022-03-09 14:55:59 +0530

    updated href of <a>

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       
-        <a className="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+        <a className="sidebar-brand d-flex align-items-center justify-content-center" href="wrapper">
         <div className="sidebar-brand-icon rotate-n-15">
             <i className="fas fa-laugh-wink"></i>
         </div>
@@ -23,7 +23,7 @@ function App() {
 
        
         <li className="nav-item active">
-        <a className="nav-link" href="#">
+        <a className="nav-link" href="wrapper">
             <i className="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
         </li>
@@ -38,32 +38,32 @@ function App() {
 
        
         <li className="nav-item">
-        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+        <a className="nav-link collapsed" href="wrapper" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
             <i className="fas fa-fw fa-cog"></i>
             <span>Components</span>
         </a>
         <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div className="bg-white py-2 collapse-inner rounded">
             <h6 className="collapse-header">Custom Components:</h6>
-            <a className="collapse-item" href="#">Buttons</a>
-            <a className="collapse-item" href="#">Cards</a>
+            <a className="collapse-item" href="wrapper">Buttons</a>
+            <a className="collapse-item" href="wrapper">Cards</a>
             </div>
         </div>
         </li>
 
        
         <li className="nav-item">
-        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+        <a className="nav-link collapsed" href="wrapper" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
             <i className="fas fa-fw fa-wrench"></i>
             <span>Utilities</span>
         </a>
         <div id="collapseUtilities" className="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
             <div className="bg-white py-2 collapse-inner rounded">
             <h6 className="collapse-header">Custom Utilities:</h6>
-            <a className="collapse-item" href="#">Colors</a>
-            <a className="collapse-item" href="#">Borders</a>
-            <a className="collapse-item" href="#">Animations</a>
-            <a className="collapse-item" href="#">Other</a>
+            <a className="collapse-item" href="wrapper">Colors</a>
+            <a className="collapse-item" href="wrapper">Borders</a>
+            <a className="collapse-item" href="wrapper">Animations</a>
+            <a className="collapse-item" href="wrapper">Other</a>
             </div>
         </div>
         </li>
@@ -78,34 +78,34 @@ function App() {
 
        
         <li className="nav-item">
-        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+        <a className="nav-link collapsed" href="wrapper" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
             <i className="fas fa-fw fa-folder"></i>
             <span>Pages</span>
         </a>
         <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
             <div className="bg-white py-2 collapse-inner rounded">
             <h6 className="collapse-header">Login Screens:</h6>
-            <a className="collapse-item" href="#">Login</a>
-            <a className="collapse-item" href="#">Register</a>
-            <a className="collapse-item" href="#">Forgot Password</a>
+            <a className="collapse-item" href="wrapper">Login</a>
+            <a className="collapse-item" href="wrapper">Register</a>
+            <a className="collapse-item" href="wrapper">Forgot Password</a>
             <div className="collapse-divider"></div>
             <h6 className="collapse-header">Other Pages:</h6>
-            <a className="collapse-item" href="#">404 Page</a>
-            <a className="collapse-item" href="#">Blank Page</a>
+            <a className="collapse-item" href="wrapper">404 Page</a>
+            <a className="collapse-item" href="wrapper">Blank Page</a>
             </div>
         </div>
         </li>
 
        
         <li className="nav-item">
-        <a className="nav-link" href="#">
+        <a className="nav-link" href="wrapper">
             <i className="fas fa-fw fa-chart-area"></i>
             <span>Charts</span></a>
         </li>
 
         
         <li className="nav-item">
-        <a className="nav-link" href="#">
+        <a className="nav-link" href="wrapper">
             <i className="fas fa-fw fa-table"></i>
             <span>Tables</span></a>
         </li>
@@ -150,7 +150,7 @@ function App() {
 
            
             <li className="nav-item dropdown no-arrow d-sm-none">
-                <a className="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a className="nav-link dropdown-toggle" href="wrapper" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i className="fas fa-search fa-fw"></i>
                 </a>
                
@@ -170,7 +170,7 @@ function App() {
 
            
             <li className="nav-item dropdown no-arrow mx-1">
-                <a className="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a className="nav-link dropdown-toggle" href="wrapper" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i className="fas fa-bell fa-fw"></i>
                
                 <span className="badge badge-danger badge-counter">3+</span>
@@ -180,7 +180,7 @@ function App() {
                 <h6 className="dropdown-header">
                     Alerts Center
                 </h6>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="wrapper">
                     <div className="mr-3">
                     <div className="icon-circle bg-primary">
                         <i className="fas fa-file-alt text-white"></i>
@@ -191,7 +191,7 @@ function App() {
                     <span className="font-weight-bold">A new monthly report is ready to download!</span>
                     </div>
                 </a>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="wrapper">
                     <div className="mr-3">
                     <div className="icon-circle bg-success">
                         <i className="fas fa-donate text-white"></i>
@@ -202,7 +202,7 @@ function App() {
                     $290.29 has been deposited into your account!
                     </div>
                 </a>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="wrapper">
                     <div className="mr-3">
                     <div className="icon-circle bg-warning">
                         <i className="fas fa-exclamation-triangle text-white"></i>
@@ -213,13 +213,13 @@ function App() {
                     Spending Alert: We've noticed unusually high spending for your account.
                     </div>
                 </a>
-                <a className="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                <a className="dropdown-item text-center small text-gray-500" href="wrapper">Show All Alerts</a>
                 </div>
             </li>
 
            
             <li className="nav-item dropdown no-arrow mx-1">
-                <a className="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a className="nav-link dropdown-toggle" href="wrapper" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i className="fas fa-envelope fa-fw"></i>
                
                 <span className="badge badge-danger badge-counter">7</span>
@@ -229,7 +229,7 @@ function App() {
                 <h6 className="dropdown-header">
                     Message Center
                 </h6>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="wrapper">
                     <div className="dropdown-list-image mr-3">
                     <img className="rounded-circle" src="https://source.unsplash.com/fn_BT9fwg_E/60x60" alt="" />
                     <div className="status-indicator bg-success"></div>
@@ -239,7 +239,7 @@ function App() {
                     <div className="small text-gray-500">Emily Fowler · 58m</div>
                     </div>
                 </a>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="wrapper">
                     <div className="dropdown-list-image mr-3">
                     <img className="rounded-circle" src="https://source.unsplash.com/AU4VPcFN4LE/60x60" alt="" />
                     <div className="status-indicator"></div>
@@ -249,7 +249,7 @@ function App() {
                     <div className="small text-gray-500">Jae Chun · 1d</div>
                     </div>
                 </a>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="wrapper">
                     <div className="dropdown-list-image mr-3">
                     <img className="rounded-circle" src="https://source.unsplash.com/CS2uCrpNzJY/60x60" alt="" />
                     <div className="status-indicator bg-warning"></div>
@@ -259,7 +259,7 @@ function App() {
                     <div className="small text-gray-500">Morgan Alvarez · 2d</div>
                     </div>
                 </a>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="wrapper">
                     <div className="dropdown-list-image mr-3">
                     <img className="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="" />
                     <div className="status-indicator bg-success"></div>
@@ -269,7 +269,7 @@ function App() {
                     <div className="small text-gray-500">Chicken the Dog · 2w</div>
                     </div>
                 </a>
-                <a className="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
+                <a className="dropdown-item text-center small text-gray-500" href="wrapper">Read More Messages</a>
                 </div>
             </li>
 
@@ -277,26 +277,26 @@ function App() {
 
             
             <li className="nav-item dropdown no-arrow">
-                <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a className="nav-link dropdown-toggle" href="wrapper" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span className="mr-2 d-none d-lg-inline text-gray-600 small">Hi John</span>
-                <img className="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60" />
+                <img className="img-profile rounded-circle" alt="porfile-pic" src="https://source.unsplash.com/QAB-WJcbgJk/60x60" />
                 </a>
               
                 <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a className="dropdown-item" href="#">
+                <a className="dropdown-item" href="wrapper">
                     <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
                 </a>
-                <a className="dropdown-item" href="#">
+                <a className="dropdown-item" href="wrapper">
                     <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                     Settings
                 </a>
-                <a className="dropdown-item" href="#">
+                <a className="dropdown-item" href="wrapper">
                     <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                     Activity Log
                 </a>
                 <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <a className="dropdown-item" href="wrapper" data-toggle="modal" data-target="#logoutModal">
                     <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
                 </a>
@@ -312,7 +312,7 @@ function App() {
         
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
-            <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i className="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+            <a href="wrapper" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i className="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
             </div>
 
             
@@ -406,15 +406,15 @@ function App() {
                 <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 className="m-0 font-weight-bold text-primary">Earnings Overview</h6>
                     <div className="dropdown no-arrow">
-                    <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a className="dropdown-toggle" href="wrapper" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                     </a>
                     <div className="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
                         <div className="dropdown-header">Dropdown Header:</div>
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
+                        <a className="dropdown-item" href="wrapper">Action</a>
+                        <a className="dropdown-item" href="wrapper">Another action</a>
                         <div className="dropdown-divider"></div>
-                        <a className="dropdown-item" href="#">Something else here</a>
+                        <a className="dropdown-item" href="wrapper">Something else here</a>
                     </div>
                     </div>
                 </div>
@@ -434,15 +434,15 @@ function App() {
                 <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 className="m-0 font-weight-bold text-primary">Revenue Sources</h6>
                     <div className="dropdown no-arrow">
-                    <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a className="dropdown-toggle" href="wrapper" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                     </a>
                     <div className="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
                         <div className="dropdown-header">Dropdown Header:</div>
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
+                        <a className="dropdown-item" href="wrapper">Action</a>
+                        <a className="dropdown-item" href="wrapper">Another action</a>
                         <div className="dropdown-divider"></div>
-                        <a className="dropdown-item" href="#">Something else here</a>
+                        <a className="dropdown-item" href="wrapper">Something else here</a>
                     </div>
                     </div>
                 </div>
@@ -636,7 +636,7 @@ function App() {
         <div className="modal-body">Select "Logout" below if you are ready to end your current session.</div>
         <div className="modal-footer">
             <button className="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a className="btn btn-primary" data-dismiss="modal" href="#">Logout</a>
+            <a className="btn btn-primary" data-dismiss="modal" href="wrapper">Logout</a>
         </div>
         </div>
     </div>

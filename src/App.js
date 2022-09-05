10db59409f36d7893aefcc1d65dce77ff5e3ae3f
Ahmad Salman Malik <36794358+ASalman-Malik@users.noreply.github.com>
2022-09-05 11:24:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       
-        <a className="sidebar-brand d-flex align-items-center justify-content-center">
+        <a className="sidebar-brand d-flex align-items-center justify-content-center" href="#">
         <div className="sidebar-brand-icon rotate-n-15">
             <i className="fas fa-laugh-wink"></i>
         </div>
